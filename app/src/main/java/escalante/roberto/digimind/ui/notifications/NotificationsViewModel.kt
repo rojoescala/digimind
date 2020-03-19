e7d7b776c5512c764197813f142478f3600cc16f
rojoescala <rojo.escala@gmail.com>
2020-03-18 17:20:35 -0800

    first commit

---
+++ app/src/main/java/escalante/roberto/digimind/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package escalante.roberto.digimind.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
